a7aee663eb2411040373b2c3e434d26b6915bd4e
jonaygarcia <jonay.garcia@gmail.com>
2017-12-14 21:03:56 +0000

    Se añade nuevo mensaje

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, World");
-		System.out.println("Hola, Mundo");
+		System.out.println("Hola, Mundo 2");
+		System.out.println("Hola, Mundo 2");
 
 	}
 
